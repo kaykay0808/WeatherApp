9625b2136b1fb97609155a93afb5e208d0e327ba
kay <kayshiro@live.no>
2024-10-13 10:59:22 +0200

    All Dependencies added
    - Okhttp
    - Room
    - Coil
    - Icon extended
    - coroutines

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,13 +62,31 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
     // Dagger/Hilt
     implementation(libs.hilt.android)
     ksp(libs.hilt.compiler)
     implementation(libs.hilt.navigation)
+
     // Retrofit
     implementation(libs.retrofit)
     implementation(libs.retrofit.gson)
+
+    // Okhttp
+    implementation(libs.squareup.okhttp)
+
+    // Room
+    implementation(libs.room)
+
+    // implementation(libs.room.annotation)
+    implementation(libs.room.androidx)
+    ksp(libs.room.annotation) // don't know if this work
+
+    // Coil
+    implementation(libs.coil.compose)
+
+    // Icon extended
+    implementation(libs.androidx.material.icons.extended)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
@@ -78,34 +96,20 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 
-    // retrofit & json converter
-    // val rfVers = "2.9.0"
-    // implementation("com.squareup.retrofit2:retrofit:$rfVers")
-    // implementation("com.squareup.retrofit2:converter-gson:$rfVers")
 
-    // OkHttp
-    // implementation("com.squareup.okhttp3:okhttp:4.12.0") // 5.0.0-alpha.2 in course
-
-    // Coil
-    // implementation("io.coil-kt:coil-compose:2.5.0")
-
-    // Room
-    // val roomVers = "2.6.1"
-    // implementation("androidx.room:room-runtime:$roomVers")
-    // annotationProcessor("androidx.room:room-compiler:$roomVers")
-    // ksp("androidx.room:room-compiler:$roomVers")
-    // implementation("androidx.room:room-ktx:$roomVers")
 
     // coroutines
-    // val crVers = "1.7.3" // 1.5.2 in course
-    // implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${crVers}")
-    // implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:${crVers}")
-    // implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:${crVers}")
+    val coroutinesVersion = "1.7.3" // 1.5.2 in course
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.kotlinx.coroutines.play.services)
     // coroutine lifecycle scopes
-    // implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0") //2.4.0
+    // implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.6")
+    implementation(libs.androidx.lifecycle.viewmodel.ktx) //2.4.0
 
     // material icons
     // implementation("androidx.compose.material:material-icons-extended:1.5.4")
+
 
 
 
