e026c6f153cde8c373da33c6a9c7ae18f9763e98
kay <kayshiro@live.no>
2024-10-15 15:07:12 +0200

    SplashScreen design

--- app/src/main/java/com/example/weatherapp/ui/screens/WeatherSplashScreen.kt
+++ app/src/main/java/com/example/weatherapp/ui/screens/WeatherSplashScreen.kt
@@ -1,11 +1,65 @@
 package com.example.weatherapp.ui.screens
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.example.weatherapp.R
 
 @Composable
 fun WeatherSplashScreen(navController: NavController) {
-    Text(text = "SPLASHSCREEN")
+    Surface(
+        modifier = Modifier
+            .padding(15.dp)
+            .size(330.dp),
+        shape = CircleShape,
+        color = Color.White,
+        border = BorderStroke(
+            width = 2.dp,
+            color = Color.LightGray
+        )
 
+    ) {
+        Column(
+            modifier = Modifier.padding(1.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                modifier = Modifier
+                    .size(95.dp),
+                contentScale = ContentScale.Fit,
+                painter = painterResource(id = R.drawable.sun),
+                contentDescription = "Sun icon"
+            )
+            Text(
+                text = "Hello Sun!",
+                style = MaterialTheme.typography.titleLarge, // h5
+                color = Color.LightGray
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun WeatherSplashScreenPreview() {
+    val navController = rememberNavController()
+    WeatherSplashScreen(navController = navController)
 }
